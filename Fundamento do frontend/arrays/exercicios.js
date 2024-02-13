9b7ad0cac2a7c099fd90940206ff4f9dc09af589
Victor Matheus <victormatheus507@gmail.com>
2024-02-13 12:20:02 -0300

    exercicios array

---
+++ Fundamento do frontend/arrays/exercicios.js
@@ -0,0 +1,23 @@
+/* 
+  1- Crie um programa que dado um número imprima a sua tabuada
+*/
+const numero = 5
+for (let i = 0; i <= 10; i++) {
+  console.log(i * numero);
+} 
+
+/* 
+  2- Crie um programa que seja capaz de percorrer uma lista de números e imprima
+  cada número P encontrado
+*/
+
+const numeros = [0,1,2,3,4,5,6,7,8,9,10]
+
+for (let i = 0; i < numeros.length; i++) {
+  const numero = numeros[i]
+
+  if(numero % 2 === 0) {
+    console.log(numero);
+  }
+  
+}
