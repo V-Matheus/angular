7ebc66ca702fea68b65818eb43c9f31438532687
Victor Matheus <victormatheus507@gmail.com>
2024-02-13 11:22:14 -0300

    ex 3

--- Fundamento do frontend/objetos e classes/exercicios.js
+++ Fundamento do frontend/objetos e classes/exercicios.js
@@ -49,6 +49,22 @@ class Pessoa {
   calcularImc() {
     return this.peso / (this.altura ** 2)
   }
+
+  classificarImc() {
+    const imc = this.calcularImc()
+
+    if (imc < 18.5) {
+      return ('Abaixo do peso');
+    } else if (imc >= 18.5 && imc < 25) {
+      return ('Peso normal');
+    } else if (imc >= 25 && imc < 30) {
+      return ('Acima do peso');
+    } else if (imc >= 30 && imc < 40) {
+      return ('Obeso');
+    } else {
+      return ('Obessidade Grave');
+    }
+  }
 }
 
 const jose = new Pessoa('jose', 70, 1.75)
