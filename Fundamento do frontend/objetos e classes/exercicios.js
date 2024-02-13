3b013f1aa6ca179a1aace6ab3cd142637d9763cc
Victor Matheus <victormatheus507@gmail.com>
2024-02-13 11:18:51 -0300

    ex 2

--- Fundamento do frontend/objetos e classes/exercicios.js
+++ Fundamento do frontend/objetos e classes/exercicios.js
@@ -25,3 +25,31 @@ class Carro {
 const uno = new Carro('Fiat', 'prata', 1/12)
 
 console.log(uno.calcularViagem(70, 5));
+
+
+/* 
+  Crie um classe para representar pessoas.
+  Para cada pessoa teremos os atrubutos nome, peso e altura.
+  As pessoas devem ter a capacidade de dizer o valor de seu IMC (IMC = peso / (altura ** 2))
+  Instancie uma pessoa chamada José que tenha 70Kg de peso e 1,75 de altura e peça a José para dizer
+  o valor do seu IMC
+*/
+
+class Pessoa {
+  nome;
+  peso;
+  altura;
+
+  constructor(nome, peso, altura) {
+    this.nome = nome;
+    this.peso = peso;
+    this.altura = altura;
+  }
+
+  calcularImc() {
+    return this.peso / (this.altura ** 2)
+  }
+}
+
+const jose = new Pessoa('jose', 70, 1.75)
+console.log(jose);
