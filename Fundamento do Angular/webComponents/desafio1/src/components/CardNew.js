6e721c3f91ea71972866129c38260981fc5c3005
Victor Matheus <victormatheus507@gmail.com>
2024-02-16 17:03:50 -0300

    construindo as props de cada news - desafio 1

--- Fundamento do Angular/webComponents/desafio1/src/components/CardNew.js
+++ Fundamento do Angular/webComponents/desafio1/src/components/CardNew.js
@@ -15,8 +15,13 @@ class CardNews extends HTMLElement {
     cardLeft.setAttribute('class', 'cardLeft');
 
     const autor = document.createElement('span');
+    autor.textContent = 'By ' + (this.getAttribute('autor') || 'Anonymous');
+
     const linkTitle = document.createElement('a');
+    linkTitle.textContent = this.getAttribute('title');
+
     const newsContent = document.createElement('p');
+    newsContent.textContent = this.getAttribute('content');
 
     cardLeft.appendChild(autor);
     cardLeft.appendChild(linkTitle);
@@ -26,7 +31,7 @@ class CardNews extends HTMLElement {
     cardRight.setAttribute('class', 'cardRight');
 
     const newsImage = document.createElement('img');
-    cardRight.appendChild(newsImage)
+    cardRight.appendChild(newsImage);
 
     componentRoot.appendChild(cardLeft);
     componentRoot.appendChild(cardRight);
