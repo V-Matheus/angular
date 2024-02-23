c09900f362b1a8d3c3dcf4a480adb0ff01536504
Victor Matheus <victormatheus507@gmail.com>
2024-02-23 20:16:09 -0300

    navegando com routerLink

--- Fundamento do Angular/components angular/rotas/src/app/app.component.ts
+++ Fundamento do Angular/components angular/rotas/src/app/app.component.ts
@@ -3,11 +3,12 @@ import { RouterOutlet } from '@angular/router';
 
 import { IndexModule } from './pages/index/index.module';
 import { PortfolioModule } from './pages/portfolio/portfolio.module';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, IndexModule, PortfolioModule],
+  imports: [RouterOutlet, IndexModule, PortfolioModule, MenuComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
