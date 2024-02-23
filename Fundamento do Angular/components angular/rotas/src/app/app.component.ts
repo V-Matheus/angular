c53a400074870f971c1c4009dc23be654b1ec7a0
Victor Matheus <victormatheus507@gmail.com>
2024-02-23 20:07:32 -0300

    anotomias de rotas

--- Fundamento do Angular/components angular/rotas/src/app/app.component.ts
+++ Fundamento do Angular/components angular/rotas/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
+import { IndexModule } from './pages/index/index.module';
+import { PortfolioModule } from './pages/portfolio/portfolio.module';
+
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, IndexModule, PortfolioModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
