3d43c5077f4f0dd211d958d1bb253f6aa8c268a0
Victor Matheus <victormatheus507@gmail.com>
2024-02-23 21:21:38 -0300

    deixando o component inteligente

--- Fundamento do Angular/components angular/services/src/app/components/card/card.component.ts
+++ Fundamento do Angular/components angular/services/src/app/components/card/card.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [CommonModule ],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent {
-
+  name:string = 'Bulbassaur'
+  attributesTypes:string[] = ['FIRE', 'ROCK']
 }
