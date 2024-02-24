6ee381939cf60cdacbc790ca731570004932e6bd
Victor Matheus <victormatheus507@gmail.com>
2024-02-23 22:12:02 -0300

    trabalhando com modulo http

--- Fundamento do Angular/components angular/services/src/app/app.component.ts
+++ Fundamento do Angular/components angular/services/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 
 import { CardComponent } from './components/card/card.component';
+import { HttpClientModule } from '@angular/common/http';
+import { PokemonService } from './services/pokemon.service';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +12,8 @@ import { CardComponent } from './components/card/card.component';
   <h1>Aula de services</h1>
   <app-card></app-card>
   <router-outlet></router-outlet>`,
-  imports: [RouterOutlet, CardComponent],
+  imports: [RouterOutlet, CardComponent, HttpClientModule],
+  providers:[PokemonService],
   styleUrl: './app.component.css'
 })
 export class AppComponent {
--- Fundamento do Angular/components angular/services/src/app/components/card/card.component.ts
+++ Fundamento do Angular/components angular/services/src/app/components/card/card.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { PokemonService } from '../../services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -8,7 +9,14 @@ import { CommonModule } from '@angular/common';
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   name:string = 'Bulbassaur'
   attributesTypes:string[] = ['FIRE', 'ROCK']
+
+  constructor(private service:PokemonService) {
+
+  }
+  ngOnInit(): void {
+      this.service.getPokemon('pikachu')
+  }
 }
